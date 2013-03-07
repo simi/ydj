8302da7adfe9b46053b8f6c21c92644bbb0a6a61
Josef Šimánek <retro@ballgag.cz>
2013-03-08 00:07:18 +0100

    fixes, version bump

--- lib/ydj.rb
+++ lib/ydj.rb
@@ -7,9 +7,7 @@ module Ydj
     page = 1
     while urls = client.videos_by(:user => channel, :per_page => 50, :page => page).videos.collect{|v| v.player_url} and urls.any?
       puts "Page #{page}, found #{urls.count} videos"
-      urls.each do |url|
-        `youtube-dl --audio-quality 320k --max-quality 22 -o '%(title)s.%(ext)s' -x --audio-format mp3 #{url}`
-      end
+      system("youtube-dl --audio-quality 320k --max-quality 22 -o '%(title)s.%(ext)s' -x --audio-format mp3 \"#{urls.join(' ')}\"")
       page = page + 1
     end
   end
