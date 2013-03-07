299acdd0fcb87a5ba8ea6d4626d45f71a03f3764
Josef Šimánek <retro@ballgag.cz>
2013-03-07 23:58:50 +0100

    Convert ony by one. Version bump.

--- lib/ydj.rb
+++ lib/ydj.rb
@@ -8,7 +8,7 @@ module Ydj
     while urls = client.videos_by(:user => channel, :per_page => 50, :page => page).videos.collect{|v| v.player_url} and urls.any?
       puts "Page #{page}, found #{urls.count} videos"
       urls.each do |url|
-        system("youtube-dl --audio-quality 320k --max-quality 22 -o '%(title)s.%(ext)s' -x --audio-format mp3 #{url}")
+        `youtube-dl --audio-quality 320k --max-quality 22 -o '%(title)s.%(ext)s' -x --audio-format mp3 #{url}`
       end
       page = page + 1
     end
--- lib/ydj/version.rb
+++ lib/ydj/version.rb
@@ -1,3 +1,3 @@
 module Ydj
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
