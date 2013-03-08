d8a5819bccf43310ebc9517a917aca6e8205ad61
Josef Šimánek <retro@ballgag.cz>
2013-03-08 08:54:45 +0100

    Final solution.

--- lib/ydj.rb
+++ lib/ydj.rb
@@ -5,10 +5,14 @@ module Ydj
     client = YouTubeIt::Client.new
     puts "Downloading #{channel}..."
     page = 1
+    array = []
     while urls = client.videos_by(:user => channel, :per_page => 50, :page => page).videos.collect{|v| v.player_url} and urls.any?
       puts "Page #{page}, found #{urls.count} videos"
-      system("youtube-dl --audio-quality 320k --max-quality 22 -o '%(title)s.%(ext)s' -x --audio-format mp3 \"#{urls.join(' ')}\"")
+      urls.each do |url|
+        array << url
+      end
       page = page + 1
     end
+    File.open('list', 'w') {|f| f.write(array.join("\n"))}
   end
 end
--- lib/ydj/version.rb
+++ lib/ydj/version.rb
@@ -1,3 +1,3 @@
 module Ydj
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
